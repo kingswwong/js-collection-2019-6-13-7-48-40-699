6f9d9a0a67ef9091679173947b9271778655c4ce
“Dean” <467271883@qq.com>
2019-07-10 13:56:28 +0800

    impletement choose_multiples_of_three

--- practices/filter/choose_multiples_of_three.js
+++ practices/filter/choose_multiples_of_three.js
@@ -1,8 +1,9 @@
 'use strict';
 
 function choose_multiples_of_three(collection) {
-
-  //implement here
+  return collection.filter(item => {
+    return item % 3 == 0;
+  })
 }
 
 module.exports = choose_multiples_of_three;
